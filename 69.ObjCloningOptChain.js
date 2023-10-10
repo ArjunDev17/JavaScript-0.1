28cb7e13cc85b001cbf38810b4d8074afd0a7379
ArjunSingh757 <arjunsingh7743@gmail.com>
2023-10-11 00:02:05 +0530

    promise in JS

--- 69.ObjCloningOptChain.js
+++ 69.ObjCloningOptChain.js
@@ -1,27 +1,27 @@
 console.log("Jai Shree Ram:Object CLoning");
-const obj={
-    key1:"value1",
-    key2:"value2"
-}
-const obj2=obj;
+const obj = {
+  key1: "value1",
+  key2: "value2",
+};
+const obj2 = obj;
 console.log(obj);
-obj.key3="value3"
+obj.key3 = "value3";
 console.log(obj2);
 //Object Cloning
-const obj3={...obj};
+const obj3 = { ...obj };
 console.log(obj);
-obj.key4="value4"
+obj.key4 = "value4";
 console.log(obj3);
 
-const obj5=Object.assign({},obj)
-console.log("Object :",obj5);
+const obj5 = Object.assign({}, obj);
+console.log("Object :", obj5);
 
 console.log("-----------------------------");
 
-const stu={
-    "name":"Kabeer",
-    "subject":{sub1:"Hindi",sub2:"Englis"}
-}
+const stu = {
+  name: "Kabeer",
+  subject: { sub1: "Hindi", sub2: "Englis" },
+};
 console.log(stu);
 console.log(stu.name);
 console.log(stu.subject.sub1);
