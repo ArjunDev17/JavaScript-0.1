bf12716606c4b3765454551d27e6a2fc1b21749a
ArjunSingh757 <arjunsingh7743@gmail.com>
2023-10-11 23:49:44 +0530

    basic

--- 1PromiseJS/promise.js
+++ 1PromiseJS/promise.js
@@ -1,1 +1,10 @@
 console.log("Jai Shree Ram");
+const myPromise = new Promise(function (resolove, reject) {
+  setTimeout(function () {
+    console.log("function executed");
+  });
+}, 1000);
+
+myPromise.then(function () {
+  console.log("Promise executed");
+});
